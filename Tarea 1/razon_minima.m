34bc03059e132bf80bff9d2eb8439360b7c44b34
Camilo Fernandez <62482962+DarkfireT09@users.noreply.github.com>
2021-08-21 15:38:36 -0500

    Update razon_minima.m
    ciclaje

--- Tarea 1/razon_minima.m
+++ Tarea 1/razon_minima.m
@@ -1,10 +1,43 @@
-function index = razon_minima(X_b, Y_k)
-    x_k = X_b./Y_k
-    s = size(x_k)
+%function index = razon_minima(X_b, Y_k)
+%    x_k = X_b./Y_k
+%    s = size(x_k)
+%    for i = 1:s(1)
+%        if x_k(i) < 0
+%            x_k(i) = inf
+%        end
+%    end
+%    [mx_k, index] = min(x_k)
+%end
+function index = razon_minima(X_b, Y_k, B, A, IB, n)
+    s = size(Y_k)
     for i = 1:s(1)
-        if x_k(i) < 0
-            x_k(i) = inf
+        if Y_k(i) < 0
+            Y_k(i) = 0
         end
     end
-    [mx_k, index] = min(x_k)
+    x_k = X_b./Y_k
+    s = size(x_k)
+    index = []
+    min = inf
+    for i = 1:s(1)
+        if x_k(i) < min
+            index = []
+            min = x_k(i)
+            index(size(index) + 1) = i
+        elseif x_k(i) == min
+            index(size(index) + 1) = i
+        end
+    end
+    
+    s = size(index)
+    if s(2) > 1
+        n = n + 1
+        s = index(n)
+        d = IB(s)
+        Y_n = inv(B)*A(:,d)
+        index = razon_minima(Y_n, Y_k, B, A, IB, n)
+    end
+    
+    return
+%    [mx_k, index] = min(x_k)
 end
