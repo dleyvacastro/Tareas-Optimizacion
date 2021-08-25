0a4a2be1c7edcdca9fcb2c861d203a1fd1a54a89
dleyvacastro <dleyvacastro@gmail.com>
2021-08-25 14:50:29 -0500

    acepta problemas de maximos

--- Tarea 1/main.m
+++ Tarea 1/main.m
@@ -1,7 +1,11 @@
 % Input del problema:
-mode = 1;
+mode = 1; % 0 - Silencioso 1 - Verbose.
+problem_mode = 0; % 0 - min 1 - max.
 A = [1,2,1,0;-1,1,0,1]
 c = [-2,-4,0,0]
+if problem_mode
+    c = -c;
+end
 b = [4;1]
 
 x_opt = simplex(A,b,c, mode)
