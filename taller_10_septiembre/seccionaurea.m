51a16d1b0c612200dd9c0936f72e1a7ea03fd9b9
Camilo Fernandez <camilof002@gmail.com>
2021-09-10 08:17:54 -0500

    taller

---
+++ taller_10_septiembre/seccionaurea.m
@@ -0,0 +1,16 @@
+function l = seccionaurea(a, b)
+
+    l = a + (1- 0.618)*(b - a)
+    u = a + (0.618)*(b-a)
+    
+    if b - a < 0.0000001
+        
+    else
+        if f(l) > f(u)
+            
+        else
+        
+        end
+    end
+    
+end
